36120c835fb4a0a873d8ce9bc50e9d3ff6524653
root <root@localhost.localdomain>
2026-06-28 01:56:27 +0000

    Fix runtime crash: move buildinfo symbols to stubs.cc, remove buildinfo.c

--- app/src/main/cpp/creator/stubs.cc
+++ app/src/main/cpp/creator/stubs.cc
@@ -11,3 +11,21 @@ void USD_ensure_plugin_path_registered(void) {
 }
 
 }
+
+extern "C" {
+
+unsigned long build_commit_timestamp = 0;
+char build_commit_date[16] = "2024-01-01";
+char build_commit_time[16] = "00:00:00";
+char build_date[] = "2024-01-01";
+char build_time[] = "00:00:00";
+char build_hash[] = "stub";
+char build_branch[] = "stub";
+char build_platform[] = "Android";
+char build_type[] = "Release";
+char build_cflags[] = "";
+char build_cxxflags[] = "";
+char build_linkflags[] = "";
+char build_system[] = "CMake";
+
+}
