87dc4394b7e3ffd1370774d1c9a83ba2afc27335
root <root@localhost.localdomain>
2026-06-28 12:37:07 +0000

    Switch to Choreographer-based render loop
    - Remove manual EGL context creation
    - Remove --background flag from argv
    - Use setAndroidApp(false) for GHOST simple mode
    - Use Choreographer for vsync-aligned render loop
    - mainBlenderLoop called on each frame callback

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -2,11 +2,11 @@
 #include <android/log.h>
 #include <android/native_window.h>
 #include <android/native_window_jni.h>
-#include <EGL/egl.h>
-#include <GLES3/gl3.h>
+#include <android/choreographer.h>
 #include <cstdlib>
 #include <cstring>
 #include <cstdio>
+#include <atomic>
 
 #include "BLI_path_util.h"
 #include "creator/creator.h"
@@ -15,82 +15,27 @@
 #define LOGE(...) ((void)__android_log_print(ANDROID_LOG_ERROR, "BlenderNative", __VA_ARGS__))
 
 static void *gContext = nullptr;
-static EGLDisplay gEglDisplay = EGL_NO_DISPLAY;
-static EGLContext gEglContext = EGL_NO_CONTEXT;
-static EGLSurface gEglSurface = EGL_NO_SURFACE;
+static std::atomic<bool> gRunning(false);
 static ANativeWindow *gNativeWindow = nullptr;
+static AChoreographer *gChoreographer = nullptr;
 
-static bool createEglContext(ANativeWindow *window) {
-    if (gEglDisplay != EGL_NO_DISPLAY) return true;
-
-    gEglDisplay = eglGetDisplay(EGL_DEFAULT_DISPLAY);
-    if (gEglDisplay == EGL_NO_DISPLAY) {
-        LOGE("eglGetDisplay failed");
-        return false;
-    }
-
-    EGLint major, minor;
-    if (!eglInitialize(gEglDisplay, &major, &minor)) {
-        LOGE("eglInitialize failed");
-        return false;
-    }
-    LOGI("EGL version %d.%d", major, minor);
-
-    EGLint attribs[] = {
-        EGL_RENDERABLE_TYPE, EGL_OPENGL_ES3_BIT,
-        EGL_SURFACE_TYPE, EGL_WINDOW_BIT,
-        EGL_BLUE_SIZE, 8,
-        EGL_GREEN_SIZE, 8,
-        EGL_RED_SIZE, 8,
-        EGL_DEPTH_SIZE, 24,
-        EGL_NONE
-    };
-    EGLConfig config;
-    EGLint numConfigs;
-    eglChooseConfig(gEglDisplay, attribs, &config, 1, &numConfigs);
-
-    gEglSurface = eglCreateWindowSurface(gEglDisplay, config, window, nullptr);
-    if (gEglSurface == EGL_NO_SURFACE) {
-        LOGE("eglCreateWindowSurface failed");
-        return false;
-    }
-
-    EGLint ctxAttribs[] = {
-        EGL_CONTEXT_CLIENT_VERSION, 3,
-        EGL_NONE
-    };
-    gEglContext = eglCreateContext(gEglDisplay, config, EGL_NO_CONTEXT, ctxAttribs);
-    if (gEglContext == EGL_NO_CONTEXT) {
-        LOGE("eglCreateContext failed");
-        return false;
-    }
-
-    if (!eglMakeCurrent(gEglDisplay, gEglSurface, gEglSurface, gEglContext)) {
-        LOGE("eglMakeCurrent failed");
-        return false;
-    }
-
-    LOGI("EGL context created successfully");
-    return true;
+extern "C" {
+extern void setAndroidApp(bool hasApp);
+extern bool hasAndroidApp();
 }
 
-static void destroyEglContext() {
-    if (gEglDisplay != EGL_NO_DISPLAY) {
-        eglMakeCurrent(gEglDisplay, EGL_NO_SURFACE, EGL_NO_SURFACE, EGL_NO_CONTEXT);
-        if (gEglContext != EGL_NO_CONTEXT) {
-            eglDestroyContext(gEglDisplay, gEglContext);
-            gEglContext = EGL_NO_CONTEXT;
-        }
-        if (gEglSurface != EGL_NO_SURFACE) {
-            eglDestroySurface(gEglDisplay, gEglSurface);
-            gEglSurface = EGL_NO_SURFACE;
-        }
-        eglTerminate(gEglDisplay);
-        gEglDisplay = EGL_NO_DISPLAY;
+static void frameCallback(long frameTimeNanos, void *data) {
+    if (!gRunning) {
+        return;
     }
-    if (gNativeWindow) {
-        ANativeWindow_release(gNativeWindow);
-        gNativeWindow = nullptr;
+    
+    if (gContext) {
+        mainBlenderLoop(gContext);
+    }
+    
+    // Schedule next frame
+    if (gChoreographer && gRunning) {
+        AChoreographer_postFrameCallback(gChoreographer, frameCallback, nullptr);
     }
 }
 
@@ -122,15 +67,11 @@ Java_com_blender_android_MainActivity_nativeOnSurfaceCreated(JNIEnv *env, jclass
         LOGE("Failed to get native window from surface");
         return;
     }
-
-    if (!createEglContext(window)) {
-        LOGE("Failed to create EGL context");
-        ANativeWindow_release(window);
-        return;
-    }
-    LOGI("=== EGL context created");
     gNativeWindow = window;
 
+    gChoreographer = AChoreographer_getInstance();
+    
+    setAndroidApp(false);
     initialLib((void*)window);
     LOGI("=== initialLib done");
 
@@ -161,26 +102,43 @@ Java_com_blender_android_MainActivity_nativeOnSurfaceCreated(JNIEnv *env, jclass
     char blenderPath[256] = {0};
     strcat(blenderPath, strHomePath);
     strcat(blenderPath, "blender");
-    const char *argv[] = {blenderPath, "--background", "--factory-startup", "-d"};
+    const char *argv[] = {blenderPath, "--factory-startup", "-d"};
     LOGI("=== Calling mainBlenderInitial...");
-    gContext = mainBlenderInitial(4, argv);
+    gContext = mainBlenderInitial(3, argv);
     LOGI("=== Blender initialized, context: %p", gContext);
+
+    if (gContext) {
+        gRunning = true;
+        LOGI("=== Starting render loop via Choreographer");
+        AChoreographer_postFrameCallback(gChoreographer, frameCallback, nullptr);
+    }
 }
 
 JNIEXPORT void JNICALL
 Java_com_blender_android_MainActivity_nativeOnPause(JNIEnv *env, jclass clazz) {
     LOGI("nativeOnPause");
+    gRunning = false;
 }
 
 JNIEXPORT void JNICALL
 Java_com_blender_android_MainActivity_nativeOnResume(JNIEnv *env, jclass clazz) {
     LOGI("nativeOnResume");
+    if (gContext && !gRunning) {
+        gRunning = true;
+        if (gChoreographer) {
+            AChoreographer_postFrameCallback(gChoreographer, frameCallback, nullptr);
+        }
+    }
 }
 
 JNIEXPORT void JNICALL
 Java_com_blender_android_MainActivity_nativeOnDestroy(JNIEnv *env, jclass clazz) {
     LOGI("nativeOnDestroy");
-    destroyEglContext();
+    gRunning = false;
+    if (gNativeWindow) {
+        ANativeWindow_release(gNativeWindow);
+        gNativeWindow = nullptr;
+    }
 }
 
 } // extern "C"
